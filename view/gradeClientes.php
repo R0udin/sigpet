911193f1af2402d4f18c5fc2d0b83235f22479d4
CrazyTranslator <bia.curupana@gmail.com>
2019-06-06 17:01:57 -0300

    Update gradeClientes.php

--- view/gradeClientes.php
+++ view/gradeClientes.php
@@ -27,17 +27,10 @@
         <thead>
             <tr>
                 <th>Cliente</th>
-                <th>CPF</th>
-                <th>RG</th>
-                <th>Data de nascimento</th>
-                <th>Data do cadastro</th>
                 <th>Telefone</th>
                 <th>Email</th>
-                <th>Tipo do cliente</th>
-                <th>CNPJ</th>
-                <th>Referencia</th>
                 <th>Celular</th>
-
+                
                 <th><a href="?controller=ClientesController&method=criar" class="btn btn-success btn-sm">Novo</a></th>
             </tr>
         </thead>
@@ -49,16 +42,10 @@
                     ?>
                     <tr>
                         <td><?php echo $cliente->NOME_CLIENTE; ?></td>
-                        <td><?php echo $cliente->CPF_CLIENTE; ?></td>
-                        <td><?php echo $cliente->RG_CLIENTE; ?></td>
-                        <td><?php echo $cliente->DT_NASCIMENTO; ?></td>
-                        <td><?php echo $cliente->DT_CADASTRO; ?></td>
                         <td><?php echo $cliente->TELEFONE; ?></td>
                         <td><?php echo $cliente->EMAIL; ?></td>
-                        <td><?php echo $cliente->TIPO_CLIENTE; ?></td>
-                        <td><?php echo $cliente->CNPJ_CLIENTE; ?></td>
-                        <td><?php echo $cliente->REFERENCIA_CLIENTE; ?></td>
                         <td><?php echo $cliente->CELULAR; ?></td>
+                        <td><a href="?controller=ClientesController&method=relatorio&id=<?php echo $cliente->id; ?>" class="btn btn-primary btn-sm">Visualizar</a></td>
                         <td>
                             <a href="?controller=ClientesController&method=editar&id=<?php echo $cliente->id; ?>" class="btn btn-primary btn-sm">Editar</a>
                             <a href="?controller=ClientesController&method=excluir&id=<?php echo $cliente->id; ?>" class="btn btn-danger btn-sm">Excluir</a>
