53cccc83d467861e55e8d72071933f648d1297c9
CrazyTranslator <bia.curupana@gmail.com>
2019-06-06 17:04:19 -0300

    Update gradeFornecedores.php

--- view/gradeFornecedores.php
+++ view/gradeFornecedores.php
@@ -26,12 +26,6 @@
         <thead>
             <tr>
                 <th>Fornecedor</th>
-                <th>CNPJ</th>
-                <th>CPF</th>
-                <th>Inscrição estadual</th>
-                <th>Tipo</th>
-                <th>Endereço</th>
-                <th>Bairro</th>
                 <th>Cidade</th>
                 <th>UF</th>
                 <th>CEP</th>
@@ -50,18 +44,15 @@
                     ?>
                     <tr>
                         <td><?php echo $fornecedor->NOME_FORNECEDOR; ?></td>
-                        <td><?php echo $fornecedor->CNPJ_FORNECEDOR; ?></td>
-                        <td><?php echo $fornecedor->CPF_FORNECEDOR; ?></td>
-                        <td><?php echo $fornecedor->INSCRICAOESTADUAL_FORNECEDOR; ?></td>
-                        <td><?php echo $fornecedor->TIPO_FORNECEDOR; ?></td>
-                        <td><?php echo $fornecedor->NDERECO_FORNECEDOR_7; ?></td>
-                        <td><?php echo $fornecedor->BAIRRO_FORNECEDOR; ?></td>
                         <td><?php echo $fornecedor->CIDADE_FORNECEDOR; ?></td>
                         <td><?php echo $fornecedor->UF_FORNECEDOR; ?></td>
                         <td><?php echo $fornecedor->CEP_FORNECEDOR; ?></td>
                         <td><?php echo $fornecedor->EMAIL_FORNECEDOR; ?></td>
                         <td><?php echo $fornecedor->TELEFONE_FORNECEDOR; ?></td>
                         <td><?php echo $fornecedor->CELULAR_FORNECEDOR; ?></td>
+                        <td>
+                          <a href="?controller=FornecedoresController&method=relatorio&id=<?php echo $fornecedor->id; ?>" class="btn btn-primary btn-sm">Visualizar</a>
+                        </td>
                         <td>
                             <a href="?controller=FornecedoresController&method=editar&id=<?php echo $fornecedor->id; ?>" class="btn btn-primary btn-sm">Editar</a>
                             <a href="?controller=FornecedoresController&method=excluir&id=<?php echo $fornecedor->id; ?>" class="btn btn-danger btn-sm">Excluir</a>
