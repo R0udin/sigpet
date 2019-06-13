fd97bb3891a9aa7f6aa18ed2f0242960581a989f
CrazyTranslator <bia.curupana@gmail.com>
2019-06-13 14:59:40 -0300

    Update produto.php

--- model/produto.php
+++ model/produto.php
@@ -157,38 +157,13 @@ class Produto
      * @param type $id
      * @return boolean
      */
-public static function AntesDel($id)
-    {
-        $conexao = Conexao::getInstance();
-        if ($conexao->exec("SELECT COUNT(1) FROM venda_dets WHERE `PRODUTO_ID`='{$id}'"))
-        {
-            $q= $conexao->exec("SELECT COUNT(1) FROM venda_dets WHERE `PRODUTO_ID`='{$id}'");
-            $r=mysql_query($q);
-            $row=mysql_fetch_row($r);
-            if($row[0] = 0) 
-            {
-                return true;
-        } else 
-            {
-                return false;
-            }
-        }
-
-        return false;
-    }
-    
     public static function destroy($id)
     {
         
         $conexao = Conexao::getInstance();
-        antesDel = new AntesDel($id);
-        if(antesDel){
-            if ($conexao->exec("DELETE FROM produtos WHERE id='{$id}';")) {
-            return true;
-            }
-            return false;
-            }
+        if ($conexao->exec("DELETE FROM produtos a WHERE NOT EXISTS (SELECT id='{$id}' FROM venda_dets b WHERE a.id = b. PRODUTO_ID);")) {
+        return true;
+        }
         return false;
-    }
+        }
 }
-
