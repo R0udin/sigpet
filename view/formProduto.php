d60eafe3617b19335937f7654031e5d2d727b997
CrazyTranslator <bia.curupana@gmail.com>
2019-06-13 19:52:42 -0300

    Update formProduto.php

--- view/formProduto.php
+++ view/formProduto.php
@@ -184,5 +184,5 @@
         </div>
     </form>
     	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-		<script src="js/bootstrap.min.js"></script>
+		<script src="../js/bootstrap.min.js"></script>
 </div>
